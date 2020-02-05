1955d7af63aa76edb8541391c77afa2520b76324
jamesnitz <jamesanitz2@gmail.com>
2020-02-05 11:44:41 -0600

    add task button changes to close on click

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -4,7 +4,7 @@ import Task from "./Task"
 
 
 export default () => {
-  const {tasks, addTask} = useContext(TaskContext)
+  const { tasks, addTask } = useContext(TaskContext)
   const activeUser = parseInt(localStorage.getItem("user"), 10)
   const [buttonClicked, setButtonClicked] = useState(false)
   const [singleTask, setTask] = useState({})
@@ -20,22 +20,22 @@ export default () => {
       return task
     }
   })
-  
+
   let taskList = ""
   if (foundTasks < 1) {
     taskList = <>
-    <button>Add List</button>
+      <button>Add List</button>
     </>
   } else {
     taskList = <>
-    {foundTasks.map(task => {
-      return <Task key={task.id}
-              task={task} />
-    })}
+      {foundTasks.map(task => {
+        return <Task key={task.id}
+          task={task} />
+      })}
     </>
   }
 
- 
+
   const constructNewTask = () => {
     addTask({
       taskItem: singleTask.taskItem,
@@ -51,54 +51,55 @@ export default () => {
   return (
     <section>
       <h1>List</h1>
-        <button onClick={() => {
-           let trueVariable = true;
-           setButtonClicked(trueVariable)
-        }}>Add an item</button>
+      <button onClick={() => {
+        let trueVariable = true;
+        setButtonClicked(trueVariable)
+      }}>{buttonClicked ? "Close" : "Add Items"}</button>
       {buttonClicked ? (
         <>
-        <fieldset>
-        <div className="form-group">
-          <label htmlFor="taskItem">Task Info </label>
-          <input type="text" name="taskItem" required autoFocus className="form-control"
-            proptype="varchar"
-            placeholder="What needs doing"
-            defaultValue={singleTask.taskItem}
-            onChange={handleControlledInputChange}
-          />
-        </div>
-      </fieldset>
-        <fieldset>
-          <div className="form-group">
-        <label htmlFor="grade">Grade this item</label>
-        <select 
-          defaultValue=""
-          name="grade"
-          id="grade"
-          onChange={handleControlledInputChange}
-          className="form-control">
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-        </select>
+          <fieldset>
+            <div className="form-group">
+              <label htmlFor="taskItem">Task Info </label>
+              <input type="text" name="taskItem" required autoFocus className="form-control"
+                proptype="varchar"
+                placeholder="What needs doing"
+                defaultValue={singleTask.taskItem}
+                onChange={handleControlledInputChange}
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <div className="form-group">
+              <label htmlFor="grade">Grade this item</label>
+              <select
+                defaultValue="select"
+                name="grade"
+                id="grade"
+                onChange={handleControlledInputChange}
+                className="form-control">
+                <option disabled>select</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+              </select>
 
-          </div>
-        </fieldset>
-        <button type="submit" onClick={evt => {
-          evt.preventDefault(
-            constructNewTask()
-          )
-        }
-        }>Log new Task</button>
+            </div>
+          </fieldset>
+          <button type="submit" onClick={evt => {
+            evt.preventDefault(
+              constructNewTask()
+            )
+          }
+          }>Log new Task</button>
         </>
-        
-        ) : (
+
+      ) : (
           <>
-          {console.log("false")}
+            {console.log("false")}
 
-        </>
-      )}
+          </>
+        )}
       {taskList}
     </section>
   )
