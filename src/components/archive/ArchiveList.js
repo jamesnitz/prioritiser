8f8f49c16a5cbda8dd288a57e6b3d4db8fe3052e
jamesnitz <jamesanitz2@gmail.com>
2020-02-11 11:18:16 -0600

    on deletion off search keyword found tasks delete

--- src/components/archive/ArchiveList.js
+++ src/components/archive/ArchiveList.js
@@ -19,20 +19,19 @@ export default () => {
       return task
     }
   })
-  
+
+
   return (
     <section>
       <h1>Archive Page</h1>
       <div className="searchContainer">
-        <input type="text" placeholder="search by keyword" ref={keywordRef} onKeyPress={(event) => {
-          if (event.charCode === 13 && keywordRef.current.value !== "") {
+        <input type="text" defaultValue={""} placeholder="search by keyword" ref={keywordRef} onKeyDown={(event) => {
+          if (event.keyCode === 13 && keywordRef.current.value !== "") {
             setTaskContainer(
               <section>
                 {
                   CompletedUserTasks.map(task => {
-                    console.log(keywordRef.current.value)
                     if (task.taskItem.includes(keywordRef.current.value) || task.taskDetail.includes(keywordRef.current.value)) {
-                      console.log(task)
                       return <Archive key={task.id}
                         task={task} />
                     }
@@ -46,7 +45,6 @@ export default () => {
         <input type="date"></input>
         <button>Search Entry</button>
       </div>
-      {console.log(taskContainer)}
       <div className="taskContainer">{taskContainer}</div>
     </section>
   )
