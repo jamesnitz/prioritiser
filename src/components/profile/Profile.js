5c8b20f0925b2256c6e107a9f1c95663b8238cfe
jamesnitz <jamesanitz2@gmail.com>
2020-02-11 12:04:24 -0600

    added chart

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,12 +1,42 @@
-import React from "react"
+import React from 'react';
+import {Line} from 'react-chartjs-2';
 
 
 
 export default () => {
 
+
+  const data = {
+    labels: ['Completed Lists'],
+    datasets: [
+     {
+      label: 'Completion',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: []
+    }
+  ]
+};
+
   return (
     <section>
       <h1>Profile Page</h1>
+      <Line data={data} />
     </section>
   )
 }
